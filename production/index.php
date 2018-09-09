d9f26b50db6895835328d01cbff7d716b4671818
datdo <dathalongbay@gmail.com>
2018-09-09 16:09:42 +0700

    init 1

--- production/index.php
+++ production/index.php
@@ -1,9 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: datdo
- * Date: 9/9/2018
- * Time: 12:44 PM
- */
-echo __FILE__;
-echo __FILE__;
+require "vendor/autoload.php";
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+
+$path = dirname(__FILE__) . '/logs/abc.log';
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler($path, Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+
+
+
