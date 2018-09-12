a170281ef0c7920027241f5629b9c90cee854140
datdo <dathalongbay@gmail.com>
2018-09-12 18:17:50 +0700

    init 1

--- production/index.php
+++ production/index.php
@@ -1,19 +1,7 @@
 <?php
 require "vendor/autoload.php";
 
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-
-
-$path = dirname(__FILE__) . '/logs/abc.log';
-
-// create a log channel
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler($path, Logger::WARNING));
-
-// add records to the log
-$log->warning('Foo');
-$log->error('Bar');
+echo __FILE__;
 
 
 
