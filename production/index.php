62dec4082ced1b215d9afb0af7f7699bddf5e99d
datdo <dathalongbay@gmail.com>
2018-09-23 16:23:46 +0700

    init 1

--- production/index.php
+++ production/index.php
@@ -6,7 +6,19 @@
         <div class="mother-grid-inner">
             <?php require 'partial/header-main.php'; ?>
 
-            <?php require 'pages/dashboard.php'; ?>
+            <?php
+            if (isset($_GET['page'])) {
+                $page = $_GET['page'].'.php';
+                $page_path = ADMIN_PATH.'pages/'.$page;
+                $check_file_path = file_exists($page_path);
+                var_dump($check_file_path);
+                echo $page_path;
+                require $page_path;
+            } else {
+                require 'pages/404.php';
+            }
+
+            ?>
 
             <?php require 'partial/copyright.php'; ?>
         </div>
