4244c379ba3dd3c434cc51115c176dbc00f83499
datdo <dathalongbay@gmail.com>
2018-09-30 14:18:46 +0700

    init 1

--- production/app/controllers/indexController.php
+++ production/app/controllers/indexController.php
@@ -10,11 +10,30 @@ class indexController {
 
     }
 
+    public function view($view, $action, $data) {
+        ob_start();
+
+        extract($data);
+
+        if (IS_ADMIN == 1) {
+            require ADMIN_VIEW_PATH.'/'.$view.'/'.$action.'.php';
+        } else {
+            require VIEW_PATH.'/'.$view.'/'.$action.'.php';
+        }
+
+        $out = ob_get_contents();
+
+        ob_end_clean();
+
+        echo $out;
+    }
+
     /**
      * Default action
      */
     public function indexAction() {
         echo '<br>' . __METHOD__;
+        return $this->view();
     }
 
 
---
+++ production/app/views/index/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: datdo
+ * Date: 9/30/2018
+ * Time: 2:13 PM
+ */
+echo '@@@ index dashboard';
